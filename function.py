1afdb8a0b36acabce5fb218cd993df52fc65fa54
afoures <afoures@macbook.pro>
2019-03-06 20:19:57 +0100

    Added some documentation for function module

--- function.py
+++ function.py
@@ -1,13 +1,54 @@
+"""
+    function module
+    
+###############################################################################
+    
+    This module contains a Function class used to represent a polynomial.
+    
+    It can be used with the aberthMethod module to find a polynomial's roots.
+    
+"""
+
+
 class   Function():
+    """
+    Function that can represent a polynomial.
+
+    Parameters
+    ----------
+    coef: a dict objet that represent the coeficients of a polynomial, with
+        exponantiation as key and coeficient as value.
+
+    Property
+    ----------
+    degree
+    coef
+    dcoef
+
+    Methods
+    ----------
+    image
+    derivative
+
+    """
+
     def __init__(self, coef):
         self.__coef = {key: complex(value) for key, value in coef.items() if value != 0}
 
     @property
     def degree(self):
+        """
+        Return the degree of the polynomial.
+        """
         return len(self.__coef) - 1
 
     @property
     def coef(self):
+        """
+        Return a list object composed with all the polynomial's coeficients
+        shifted to have them starting with first exponantiation as 0.
+        Exponantiation for each is now the index in the list.
+        """
         indexes = list(self.__coef.keys())
         index = min(indexes)
         coef = []
@@ -22,6 +63,12 @@ class   Function():
 
     @property
     def dcoef(self):
+        """
+        Return a list object composed with all the polynomial derivative's 
+        coeficients shifted to have them starting with first exponantiation
+        as 0.
+        Exponantiation for each is now the index in the list.
+        """
         coef = self.coef
         dcoef = []
         for i, c in enumerate(coef):
@@ -29,7 +76,13 @@ class   Function():
         return dcoef[1:]
 
     def image(self, x):
+        """
+        Return the image of the polynomial for 'x'.
+        """
         return sum(coef*(pow(x, i)) for i, coef in enumerate(self.coef))
 
     def derivative(self, x):
+        """
+        Return the derivative of the polynomial for 'x'.
+        """
         return sum(coef*(pow(x, i)) for i, coef in enumerate(self.dcoef))
